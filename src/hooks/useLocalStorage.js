4eebba24d4994aa72144dec16580a4a57ab82ffe
pch2013 <pch2103@gmail.com>
2020-05-11 20:36:59 +0300

    1. Main menu 1.1. Language change 1.2. Theme switcher 1.3. Side Drawer 2. Main Page banner Deploy Netlify v0.0.1 3. Add LocalStorage 3.1 Memorize change theme 3.2 Memorize change language

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,12 +1,19 @@
 import {useEffect, useState} from 'react';
 
-export default (key, initialValue = '') => {
-	const [value, setValue] = useState(() =>
-			(localStorage.getItem(key) || initialValue)
-	)
+const loadStore = (key) => {
+	if (typeof localStorage !== 'undefined') {
+		return localStorage.getItem(key)
+	}
+	return null
+}
+
+export default (key, initialValue='') => {
+	const [value, setValue] = useState(() => (loadStore(key) || initialValue))
+
+// ( typeof localStorage !== 'undefined' && localStorage.getItem(key) || initialValue))
 
 	useEffect(() => {
-		localStorage.setItem(key, value)
+			localStorage.setItem(key, value)
 	}, [value, key])
 
 	return [value, setValue]
