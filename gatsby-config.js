8697feea51a3f893a6e45245353d5f0edfd0dbc4
pch2013 <pch2103@gmail.com>
2020-05-11 12:42:18 +0300

    1. Main menu 1.1. Language change 1.2. Theme switcher 1.3. Side Drawer 2. Main Page banner Deploy Netlify v0.0.1 - one theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
-if (process.env.NODE_ENV === "development") {
+// if (process.env.NODE_ENV === "development") {
   require("dotenv").config();
-}
+// }
 
 module.exports = {
   plugins: [
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { ThemeProvider } from '@material-ui/core/styles';
-import {theme} from '../theme';
+import Theme from '../theme';
 import MainMenu from "./mainMenu/mainMenu";
 import Copyright from "./Copyright";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -10,7 +10,7 @@ const Layout = ({ children }) => {
 
 	return (
 			<>
-				<ThemeProvider theme={theme}>
+				<ThemeProvider theme={Theme()}>
 				<CssBaseline />
 				<MainMenu />
 						{children}
